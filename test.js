b9b92d3e3220b07b604ff43637648b5c10659549
IanTsai1 <113403773+IanTsai1@users.noreply.github.com>
2023-06-13 16:44:45 +0800

    finished frontend haven't connected it to backend

--- test.js
+++ test.js
@@ -1,11 +1,6 @@
 function handleFileSelect(event) {
     const file = event.target.files[0]; // Get the selected file
-
-    // Display the file name on the page
-    const fileNameDisplay = document.getElementById("file_name_display");
-    fileNameDisplay.textContent = file.name;
-    const fileSizeDisplay = document.getElementById('file_size_display');
-    fileSizeDisplay.textContent = Math.round(file.size/1000) + " KB";
-
-    //file.size;
+    localStorage.setItem("selectedFileName", file.name);
+    localStorage.setItem("selectedFileSize", Math.round(file.size/1000) + " KB");
+    window.location.href = 'after_file.html';
   }
